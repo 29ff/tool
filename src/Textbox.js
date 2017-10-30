5309db65cfb1a442489dfa9a6b44e5a6e2b3066a
29ff <nbhung1809@gmail.com>
2017-10-30 17:46:26 +0700

    second commit

--- src/Textbox.js
+++ src/Textbox.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { findDOMNode } from 'react-dom';
-import { Button, Form, TextArea, Grid } from 'semantic-ui-react'
-import get from 'lodash.get';
+import { Button, Form, TextArea, Grid, Message } from 'semantic-ui-react'
 
 class Textbox extends Component {
   constructor(props) {
@@ -9,29 +8,55 @@ class Textbox extends Component {
 
     this.state = {
       temandoColor: '#f2612c',
-      textFocus: true
+      error: '',
+      buttonDisable: true
     }
 
     this.handleButtonClick = this.handleButtonClick.bind(this);
     this.haveOwnDeepProperty = this.haveOwnDeepProperty.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
   }
 
   haveOwnDeepProperty(obj, prop) {
     if (typeof obj === 'object' && obj !== null) {
       if (obj.hasOwnProperty(prop)) {
-        return true;
+        if (Array.isArray(obj[prop])) {
+          return obj[prop];
+        }
       }
       for (var ele in obj) {
-        if (typeof ele === 'object' && ele !== null && haveOwnDeepProperty(ele, prop)) {
-          return true;
+        console.log(ele);
+        if (typeof obj[ele] === 'object' && obj[ele] !== null && this.haveOwnDeepProperty(obj[ele], prop)) {
+          if (Array.isArray(obj[ele][prop])) {
+            return obj[ele][prop];
+          }
         }
       }
     }
     return false;
   }
 
+  handleTextChange() {
+    if (findDOMNode(this.refs.text).value) {
+      this.setState({
+        buttonDisable: false
+      })
+    } else {
+      this.setState({
+        buttonDisable: true
+      })
+    }
+  }
+
   handleButtonClick() {
-    const textValue = JSON.parse(findDOMNode(this.refs.text).value);
+    let textValue = '';
+    try {
+      textValue = JSON.parse(findDOMNode(this.refs.text).value);
+    } catch (error) {
+      this.setState({
+        error: 'Nhập không đúng định dạng'
+      })
+    }
     console.log(this.haveOwnDeepProperty(textValue, 'documentation'));
   }
 
@@ -40,14 +65,17 @@ class Textbox extends Component {
       <Grid>
         <Grid.Row centered>
           <Grid.Column width={14}>
-            <Form>
+            <Form error>
               <TextArea placeholder="Input data here..."
                         rows={12}
                         onChange={this.handleTextChange}
                         autoFocus
                         ref="text"
               />
-              <Button fluid disabled={false} style={{ backgroundColor: this.state.temandoColor, color: "#fff" }} onClick={this.handleButtonClick}>Submit</Button>
+              <Button ref="button" fluid disabled={this.state.buttonDisable} style={{ backgroundColor: this.state.temandoColor, color: "#fff" }} onClick={this.handleButtonClick}>Submit</Button>
+              {
+                (this.state.error !== '') ? <Message error content={this.state.error} /> : ''
+              }
             </Form>
           </Grid.Column>
         </Grid.Row>
